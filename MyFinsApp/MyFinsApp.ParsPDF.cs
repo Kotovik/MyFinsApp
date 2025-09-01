709a8a992538919d0c259a9f9fa9a006c3d3e4f9
Kotovik <Diago_2000@mail.ru>
2025-09-01 23:50:41 +0400

    Добавил парсинг PDF выписки со счтеа

---
+++ MyFinsApp/MyFinsApp.ParsPDF.cs
@@ -0,0 +1,39 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MyFinsApp
+{
+    public static class Para
+    {
+        
+        public static void ParsPDF()
+        {
+            using (PdfReader reader = new PdfReader(@"C:\Users\Diago\OneDrive\Desktop\test.pdf"))
+            {
+                StringBuilder text = new StringBuilder();
+                ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string page = "";
+
+                    page = PdfTextExtractor.GetTextFromPage(reader, i, Strategy);
+                    string[] lines = page.Split('\n');
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line.ToString());
+                    }
+                }
+            }
+        }
+
+    }
+}
--- MyFinsApp/Program.cs
+++ MyFinsApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿using MyFinsApp;
 using System.Data;
-    Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
 
     DateTime dat = new DateTime(2015, 7, 20);
     RealTransaction p1 = new RealTransaction(dat,123, "ffffff", 123);
 Console.WriteLine();
+
+
+Para.ParsPDF();
