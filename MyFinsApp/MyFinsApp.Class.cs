d7cd6c5390be2e32a9ab6438c109621ad65c1f8e
Diago <Diago@ANT2000>
2025-09-01 21:54:25 +0400

    Добавьте файлы проекта.

---
+++ MyFinsApp/MyFinsApp.Class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinsApp
+{
+    /// <summary>
+    /// Абстрактный для транзакций
+    /// </summary>
+    public abstract class Transaction
+    {
+        public DateTime Data { get; protected set; }
+        public int Value { get; protected set; }
+        public string PlaceOperation { get; protected set; }
+
+        protected Transaction(DateTime data, int value, string placeoperation)
+        {
+            Data = data;
+            Value = value;
+            PlaceOperation = placeoperation;
+        }
+        
+      
+    }
+    public class RealTransaction : Transaction
+    {
+        public int MSS { get; set; }
+
+        public RealTransaction(DateTime data, int value, string place_operation, int mss)
+                : base(data, value, place_operation)
+        {
+            MSS = mss;
+        }
+        public DateTime GetData()
+            { return Data; }
+        public int GetValue()
+            { return Value; }
+        public string GetPlaceOperation()
+            { return PlaceOperation; }
+        public int GetMSS() 
+        { return MSS; }
+    }
+}
+
+   
---
+++ MyFinsApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyFinsApp;
+using System.Data;
+    Console.WriteLine("Hello, World!");
+
+    DateTime dat = new DateTime(2015, 7, 20);
+    RealTransaction p1 = new RealTransaction(dat,123, "ffffff", 123);
+Console.WriteLine();
